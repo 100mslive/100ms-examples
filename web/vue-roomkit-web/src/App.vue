ec617f967e9a2a6cb6a265c2e00cdba57248a692
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-10-29 18:55:41 +0530

    fix: remove unused files

--- web/vue-roomkit-web/src/App.vue
+++ web/vue-roomkit-web/src/App.vue
@@ -1,20 +1,14 @@
 <template>
   <div id="app">
-    <hms-prebuilt
-      room-code="fba-xji-pnk"
-    ></hms-prebuilt>
+    <hms-prebuilt room-code="fba-xji-pnk"></hms-prebuilt>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import '@100mslive/roomkit-web-component';
+import "@100mslive/roomkit-web-component";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
+  name: "App",
 };
 </script>
 
