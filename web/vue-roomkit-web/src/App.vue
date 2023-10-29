cbf0bc49bb4cbcabc0a7088345794b1673c1c27f
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-10-29 18:52:50 +0530

    feat: vue example using roomkit-web

---
+++ web/vue-roomkit-web/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <hms-prebuilt
+      room-code="fba-xji-pnk"
+    ></hms-prebuilt>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import '@100mslive/roomkit-web-component';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100vh;
+}
+</style>
